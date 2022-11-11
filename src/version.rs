8147e69119dd8554b6a8f05377314e0fb71a5e52
leon3s <leo.vernisse@gmail.com>
2022-11-11 17:23:35 +0100

    [BUGFIX] Version

--- src/version.rs
+++ src/version.rs
@@ -1,7 +1,7 @@
 pub fn print_version() {
   const ARCH: &str = "amd64";
-  const VERSION: &str = "0.1.4";
-  const COMMIT_ID: &str = "1b96d659";
+  const VERSION: &str = "0.1.5";
+  const COMMIT_ID: &str = "a42e4361";
 
   println!("Arch: {}", ARCH);
   println!("Version: {}", VERSION);
