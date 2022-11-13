a8d8dc50c98fa7b34e8144e2225c40532fe1fed4
leone <leo.vernisse@gmail.com>
2022-11-13 17:14:38 +0000

    [CI/CD] Fix tests

--- scripts/prepare_ci.sh
+++ scripts/prepare_ci.sh
@@ -2,11 +2,9 @@
 ## name: ci_install.sh
 
 useradd -U nanocl;
-usermod -aG nanocl $USER;
 mkdir /etc/nanocl;
 chmod 777 /etc/nanocl;
 mkdir /run/nanocl;
 chmod 777 /run/nanocl;
-echo "docker_host: /run/docker.sock\n" > /etc/nanocl/nanocl.conf;
+echo "docker_host: /run/docker.sock" > /etc/nanocl/nanocl.conf;
 cat /etc/nanocl/nanocl.conf;
-
