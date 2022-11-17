4975165de915caa8abc5915cb525cc9a87cbbe54
leon3s <leo.vernisse@gmail.com>
2022-11-17 02:59:16 +0100

    [TEST] Added cargo image e2e tests

--- tests/version.rs
+++ tests/version.rs
@@ -1,13 +1,8 @@
 pub mod common;
 
-#[cfg(test)]
-pub mod version {
-  use super::common;
-
-  #[ntex::test]
-  async fn exec_version() -> Result<(), common::TestError> {
-    let output = common::spawn_cli(vec!["version"]).await?;
-    assert!(output.status.success());
-    Ok(())
-  }
+#[ntex::test]
+async fn exec() -> Result<(), common::TestError> {
+  let output = common::spawn_cli(vec!["version"]).await?;
+  assert!(output.status.success());
+  Ok(())
 }
