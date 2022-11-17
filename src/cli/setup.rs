58ba4e53007e0cb3ce679508b58e14435167cb9d
leon3s <leo.vernisse@gmail.com>
2022-11-17 03:50:29 +0100

    [REFACTOR] Updated daemon version

--- src/cli/setup.rs
+++ src/cli/setup.rs
@@ -24,7 +24,7 @@ use crate::config::{read_daemon_config_file, DaemonConfig};
 
 use super::errors::CliError;
 
-const DAEMON_VERSION: &str = "0.1.12";
+const DAEMON_VERSION: &str = "0.1.13";
 
 async fn instance_exists(
   name: &str,
