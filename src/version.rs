1243eb28db873d573187d839f706a6d2213345d2
leon3s <leo.vernisse@gmail.com>
2022-11-21 16:31:22 +0100

    [RELEASE] 0.1.8

--- src/version.rs
+++ src/version.rs
@@ -1,7 +1,7 @@
 pub fn print_version() {
   const ARCH: &str = "amd64";
-  const VERSION: &str = "0.1.7";
-  const COMMIT_ID: &str = "d0146f87";
+  const VERSION: &str = "0.1.8";
+  const COMMIT_ID: &str = "dac2b536";
 
   println!("Arch: {}", ARCH);
   println!("Version: {}", VERSION);
