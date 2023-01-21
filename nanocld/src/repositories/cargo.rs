41304535f0c99812d5a26d240cc967a54007eeee
leone <leo.vernisse@gmail.com>
2023-01-21 21:14:36 +0000

    [REFACTOR] Cargo inspect with optimised query

--- nanocld/src/repositories/cargo.rs
+++ nanocld/src/repositories/cargo.rs
@@ -1,14 +1,16 @@
+use nanocl_models::cargo_config::{CargoConfig, CargoConfigPartial};
 use ntex::web;
+use ntex::http::StatusCode;
 use diesel::prelude::*;
 
 use nanocl_models::cargo::Cargo;
 use nanocl_models::generic::GenericDelete;
 
-use crate::schema::cargoes;
 use crate::utils;
 use crate::error::HttpResponseError;
 use crate::models::{
   Pool, CargoPartial, CargoDbModel, NamespaceDbModel, CargoUpdateDbModel,
+  CargoConfigDbModel,
 };
 
 use super::cargo_config;
@@ -17,13 +19,25 @@ use super::error::db_blocking_error;
 /// ## Find cargo items by namespace
 ///
 /// ## Arguments
+///
 /// - [nsp](NamespaceItem) - Namespace item
 /// - [pool](Pool) - Database connection pool
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///  - [Ok](Vec<CargoDbModel>) - List a cargo found
 ///  - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// use nanocl_models::namespace::NamespaceItem;
+/// let nsp = NamespaceItem {
+///  name: String::from("test"),
+/// };
+/// let items = find_by_namespace(nsp, &pool).await;
+/// ```
 ///
 pub async fn find_by_namespace(
   nsp: NamespaceDbModel,
@@ -38,6 +52,34 @@ pub async fn find_by_namespace(
   Ok(items)
 }
 
+/// ## Create cargo
+///
+/// Create a cargo item in database for given namespace
+///
+/// ## Arguments
+///
+/// - [nsp](String) - Namespace name
+/// - [item](CargoPartial) - Cargo item
+/// - [pool](Pool) - Database connection pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](Cargo) - The cargo created
+///   - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// use nanocl_models::cargo::CargoPartial;
+///
+/// let item = CargoPartial {
+///   name: String::from("test"),
+///   //... fill required data
+/// };
+/// let cargo = create(String::from("test"), item, &pool).await;
+/// ```
+///
 pub async fn create(
   nsp: String,
   item: CargoPartial,
@@ -77,6 +119,27 @@ pub async fn create(
   Ok(cargo)
 }
 
+/// ## Delete cargo by key
+///
+/// Delete a cargo item in database for given key
+///
+/// ## Arguments
+///
+/// - [key](String) - Cargo key
+/// - [pool](Pool) - Database connection pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](GenericDelete) - The number of deleted items
+///   - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// let res = delete_by_key(String::from("test"), &pool).await;
+/// ```
+///
 pub async fn delete_by_key(
   key: String,
   pool: &Pool,
@@ -94,6 +157,27 @@ pub async fn delete_by_key(
   Ok(GenericDelete { count: res })
 }
 
+/// ## Find cargo by key
+///
+/// Find a cargo item in database for given key
+///
+/// ## Arguments
+///
+/// - [key](String) - Cargo key
+/// - [pool](Pool) - Database connection pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](CargoDbModel) - The cargo found
+///   - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// let cargo = find_by_key(String::from("test"), &pool).await;
+/// ```
+///
 pub async fn find_by_key(
   key: String,
   pool: &Pool,
@@ -109,6 +193,33 @@ pub async fn find_by_key(
   Ok(item)
 }
 
+/// ## Update cargo by key
+///
+/// Update a cargo item in database for given key
+///
+/// ## Arguments
+///
+/// - [key](String) - Cargo key
+/// - [item](CargoPartial) - Cargo item
+/// - [pool](Pool) - Database connection pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](Cargo) - The cargo updated
+///   - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// use nanocl_models::cargo::CargoPartial;
+/// let item = CargoPartial {
+///  name: String::from("test"),
+///  //... fill required data
+/// };
+/// let cargo = update_by_key(String::from("test"), item, &pool).await;
+/// ```
+///
 pub async fn update_by_key(
   key: String,
   item: CargoPartial,
@@ -147,16 +258,38 @@ pub async fn update_by_key(
   Ok(cargo)
 }
 
+/// ## Count cargo by namespace
+///
+/// Count cargo items in database for given namespace
+///
+/// ## Arguments
+///
+/// - [namespace](String) - Namespace name
+/// - [pool](Pool) - Database connection pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](i64) - The number of cargo items
+///   - [Err](HttpResponseError) - Error during the operation
+///
+/// ## Examples
+///
+/// ```rust,norun
+/// let count = count_by_namespace(String::from("test"), &pool
+/// ).await;
+/// ```
+///
 pub async fn count_by_namespace(
   namespace: String,
   pool: &Pool,
 ) -> Result<i64, HttpResponseError> {
-  use crate::schema::cargoes::dsl;
+  use crate::schema::cargoes;
 
   let mut conn = utils::store::get_pool_conn(pool)?;
   let count = web::block(move || {
     cargoes::table
-      .filter(dsl::namespace_name.eq(namespace))
+      .filter(cargoes::namespace_name.eq(namespace))
       .count()
       .get_result(&mut conn)
   })
@@ -164,4 +297,47 @@ pub async fn count_by_namespace(
   .map_err(db_blocking_error)?;
 
   Ok(count)
+}
+
+pub async fn inspect_by_key(
+  key: String,
+  pool: &Pool,
+) -> Result<Cargo, HttpResponseError> {
+  use crate::schema::cargoes;
+  use crate::schema::cargo_configs;
+
+  let mut conn = utils::store::get_pool_conn(pool)?;
+  let item: (CargoDbModel, CargoConfigDbModel) = web::block(move || {
+    cargoes::table
+      .inner_join(cargo_configs::table)
+      .filter(cargoes::key.eq(key))
+      .first(&mut conn)
+  })
+  .await
+  .map_err(db_blocking_error)?;
+
+  let config = serde_json::from_value::<CargoConfigPartial>(item.1.config)
+    .map_err(|err| HttpResponseError {
+      status: StatusCode::INTERNAL_SERVER_ERROR,
+      msg: format!("Error parsing cargo config: {}", err),
+    })?;
+
+  let config = CargoConfig {
+    key: item.1.key,
+    name: config.name,
+    cargo_key: item.1.cargo_key,
+    dns_entry: config.dns_entry,
+    replication: config.replication,
+    container: config.container,
+  };
+
+  let item = Cargo {
+    key: item.0.key,
+    name: item.0.name,
+    config_key: item.1.key,
+    namespace_name: item.0.namespace_name,
+    config,
+  };
+
+  Ok(item)
 }
--- nanocld/src/utils/cargo.rs
+++ nanocld/src/utils/cargo.rs
@@ -11,6 +11,8 @@ use crate::repositories;
 use crate::error::HttpResponseError;
 use crate::models::{Pool, CargoPartial};
 
+/// ## Create instance
+///
 /// Create containers based on the cargo config
 /// The number of containers created is based on the number of instances
 /// defined in the cargo config
@@ -23,11 +25,13 @@ use crate::models::{Pool, CargoPartial};
 /// The cargo key is used to track the containers
 ///
 /// ## Arguments
+///
 /// - [cargo](Cargo) - The cargo
 /// - [number](i64) - The number of containers to create
 /// - [docker_api](bollard::Docker) - The docker api
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](Ok) - The containers has been created
 ///   - [Err](HttpResponseError) - The containers has not been created
@@ -72,13 +76,15 @@ async fn create_instance(
   Ok(())
 }
 
-/// List containers based on the cargo key
+/// ## List containers based on the cargo key
 ///
 /// ## Arguments
+///
 /// - [cargo_key](String) - The cargo key
 /// - [docker_api](bollard::Docker) - The docker api
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](Vec<ContainerSummary>) - The containers have been listed
 ///   - [Err](HttpResponseError) - The containers have not been listed
@@ -105,15 +111,17 @@ pub async fn list_instance(
   Ok(containers)
 }
 
-/// Create a new cargo with his containers
+/// ## Create a new cargo with his containers
 ///
 /// ## Arguments
+///
 /// - [cargo_partial](CargoConfigPartial) - The cargo partial
 /// - [namespace](String) - The namespace
 /// - [docker_api](bollard::Docker) - The docker api
 /// - [pool](Pool) - The database pool
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](Cargo) - The cargo has been created
 ///   - [Err](HttpResponseError) - The cargo has not been created
@@ -139,15 +147,18 @@ pub async fn create(
   Ok(cargo)
 }
 
-/// Start containers of the given cargo
+/// ## Start containers of the given cargo
+///
 /// The containers are started in parallel
 /// If one container fails to start, the other containers will continue to start
 ///
 /// ## Arguments
+///
 /// - [cargo_key](str) - The cargo key
 /// - [docker_api](bollard::Docker) - The docker api
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](()) - The containers has been started
 ///   - [Err](HttpResponseError) - The containers has not been started
@@ -171,21 +182,18 @@ pub async fn start(
   Ok(())
 }
 
-/// Stop containers of the given cargo
+/// ## Stop containers of the given cargo
 ///
 /// ## Arguments
+///
 /// - [cargo_key](str) - The cargo key
 /// - [docker_api](bollard::Docker) - The docker api
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///  - [Ok](Ok) - The containers has been stopped
 ///  - [Err](HttpResponseError) - The containers has not been stopped
-///
-/// ## Example
-/// ```rust,norun
-/// stop("global-test", &docker_api).await.expect("Unable to stop cargo);
-/// ```
 ///
 pub async fn stop(
   cargo_key: &str,
@@ -209,19 +217,16 @@ pub async fn stop(
 /// Delete containers of the given cargo and the cargo itself
 ///
 /// ## Arguments
+///
 /// - [cargo_key](str) - The cargo key
 /// - [docker_api](bollard::Docker) - The docker api
 /// - [pool](Pool) - The database pool
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](Ok) - The cargo has been deleted
 ///   - [Err](HttpResponseError) - The cargo has not been deleted
-///
-/// ## Example
-/// ```rust,norun
-/// delete("global-test", &docker_api, &pool).await.expect("Unable to delete cargo);
-/// ```
 ///
 pub async fn delete(
   cargo_key: &str,
@@ -350,15 +355,18 @@ pub async fn patch(
   Ok(cargo)
 }
 
-/// List cargo in given namespace
+/// ## List cargo in given namespace
+///
 /// The containers are filtered by the cargo key
 ///
 /// ## Arguments
+///
 /// - [nsp](str) - The namespace name
 /// - [docker_api](bollard::Docker) - The docker api
 /// - [pool](Pool) - The database pool
 ///
 /// ## Returns
+///
 /// - [Result](Result) - The result of the operation
 ///   - [Ok](Vec<ContainerSummary>) - The containers of the cargo
 ///   - [Err](HttpResponseError) - The containers of the cargo has not been listed
@@ -400,16 +408,28 @@ pub async fn list(
   Ok(cargo_summaries)
 }
 
+/// ## Inspect cargo
+///
+/// Return information about the cargo
+///
+/// ## Arguments
+///
+/// - [key](str) - The cargo key
+/// - [docker_api](bollard::Docker) - The docker api
+/// - [pool](Pool) - The database pool
+///
+/// ## Returns
+///
+/// - [Result](Result) - The result of the operation
+///   - [Ok](CargoInspect) - The cargo information
+///   - [Err](HttpResponseError) - The cargo has not been inspected
+///
 pub async fn inspect(
   key: &str,
   docker_api: &bollard::Docker,
   pool: &Pool,
 ) -> Result<CargoInspect, HttpResponseError> {
-  let cargo = repositories::cargo::find_by_key(key.to_owned(), pool).await?;
-
-  let config =
-    repositories::cargo_config::find_by_key(cargo.config_key.to_owned(), pool)
-      .await?;
+  let cargo = repositories::cargo::inspect_by_key(key.to_owned(), pool).await?;
   let containers = list_instance(&cargo.key, docker_api).await?;
 
   let mut running_instances = 0;
@@ -424,7 +444,7 @@ pub async fn inspect(
     name: cargo.name,
     config_key: cargo.config_key,
     namespace_name: cargo.namespace_name,
-    config,
+    config: cargo.config,
     running_instances,
     containers,
   })
