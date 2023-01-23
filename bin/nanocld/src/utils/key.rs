567763816e45cbc25e96d73bf2dd570f02e8c644
leone <leo.vernisse@gmail.com>
2023-01-23 15:03:56 +0100

    [DOC] Nanocld better commentaries on utils/key.rs

--- bin/nanocld/src/utils/key.rs
+++ bin/nanocld/src/utils/key.rs
@@ -4,19 +4,24 @@
 /// For example if we create a cargo `get-started` in the default namespace `global`
 /// The cargo key will be `global-get-started`
 
-/// ## Resolve a optional namespace to his default value
+/// ## Resolve nsp
+///
+/// Resolve the namespace from the query paramater
 /// Namespace is an optional query paramater it's resolved with value `global` if it's empty
 ///
 /// ## Arguments
+///
 /// - [nsp](Option<String>) The namespace to resolve
 ///
 /// ## Return
+///
 /// - [namespace](String) The resolved namespace
 ///
 /// ## Example
 /// ```rust,norun
 /// let nsp = resolve_nsp(None); // return "global"
 /// ```
+///
 pub fn resolve_nsp(nsp: &Option<String>) -> String {
   match nsp {
     None => String::from("global"),
@@ -25,20 +30,23 @@ pub fn resolve_nsp(nsp: &Option<String>) -> String {
 }
 
 /// ## Generate key
-/// Return the generated key from 2 strings
+/// Return the generated key from the namespace and the name of an item
 ///
 /// ## Arguments
+///
 /// - [m1](str)  The key of the first model
 /// - [m2](str) The name of the second model
 ///
 /// ## Return
+///
 /// - [key](String) The generated key based on params
 ///
 /// ## Example
-/// Generate the key of network `front` in namespace `global`
+///
 /// ```rust,norun
 /// let key = gen_key("global", "front");
 /// ```
-pub fn gen_key(m1: &str, m2: &str) -> String {
-  m1.to_owned() + "-" + m2
+///
+pub fn gen_key(nsp: &str, name: &str) -> String {
+  nsp.to_owned() + "-" + name
 }
