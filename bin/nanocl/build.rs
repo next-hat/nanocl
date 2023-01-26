ac8efd502e97d89f4a9d3edcc0ee4e36c83186a1
leone <leo.vernisse@gmail.com>
2023-01-26 15:06:37 +0100

    [REFACTOR] Nanocld use build-time information (#98)

--- bin/nanocl/build.rs
+++ bin/nanocl/build.rs
@@ -1,7 +1,6 @@
-use std::{
-  fs,
-  io::{Result, ErrorKind, Error},
-};
+use std::fs;
+use std::io::{Result, Error, ErrorKind};
+
 use clap::*;
 
 include!("./src/models/mod.rs");
@@ -14,6 +13,29 @@ struct ManPage<'a> {
 
 /// Path where to generate the files
 const MAN_PATH: &str = "../../target/man";
+
+/// Set the git commit hash as an environment variable
+fn set_env_git_commit_hash() -> Result<()> {
+  let output = std::process::Command::new("git")
+    .args(["rev-parse", "HEAD"])
+    .output()?;
+
+  let git_hash = String::from_utf8(output.stdout).unwrap();
+
+  println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+  Ok(())
+}
+
+/// Set the target architecture as an environment variable
+fn set_env_target_arch() -> Result<()> {
+  let arch = std::env::var("CARGO_CFG_TARGET_ARCH")
+    .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+  println!("cargo:rustc-env=TARGET_ARCH={}", arch);
+
+  Ok(())
+}
 
 /// Function to generate a man page
 fn generate_man_page<'a>(name: &'a str, app: &'a clap::Command) -> Result<()> {
@@ -30,6 +52,7 @@ fn generate_man_page<'a>(name: &'a str, app: &'a clap::Command) -> Result<()> {
   Ok(())
 }
 
+/// Generate all man pages
 pub fn generate_man_pages() -> Result<()> {
   let man_pages: Vec<ManPage> = vec![
     ManPage {
@@ -53,30 +76,9 @@ pub fn generate_man_pages() -> Result<()> {
   Ok(())
 }
 
-fn set_env_git_commit_hash() -> Result<()> {
-  let output = std::process::Command::new("git")
-    .args(&["rev-parse", "HEAD"])
-    .output()?;
-
-  let git_hash = String::from_utf8(output.stdout).unwrap();
-
-  println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-
-  Ok(())
-}
-
-fn set_env_target_arch() -> Result<()> {
-  let arch = std::env::var("CARGO_CFG_TARGET_ARCH")
-    .map_err(|e| Error::new(ErrorKind::Other, e))?;
-
-  println!("cargo:rustc-env=TARGET_ARCH={}", arch);
-
-  Ok(())
-}
-
 fn main() -> Result<()> {
-  generate_man_pages()?;
-  set_env_git_commit_hash()?;
   set_env_target_arch()?;
+  set_env_git_commit_hash()?;
+  generate_man_pages()?;
   Ok(())
 }
--- bin/nanocld/build.rs
+++ bin/nanocld/build.rs
@@ -1,10 +1,36 @@
-use clap::*;
 use std::fs;
+use std::io::{Result, Error, ErrorKind};
+
+use clap::*;
 
 include!("./src/cli.rs");
 
 const MAN_PATH: &str = "../../target/man";
 
+/// Set the git commit hash as an environment variable
+fn set_env_git_commit_hash() -> Result<()> {
+  let output = std::process::Command::new("git")
+    .args(["rev-parse", "HEAD"])
+    .output()?;
+
+  let git_hash = String::from_utf8(output.stdout).unwrap();
+
+  println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+  Ok(())
+}
+
+/// Set the target architecture as an environment variable
+fn set_env_target_arch() -> Result<()> {
+  let arch = std::env::var("CARGO_CFG_TARGET_ARCH")
+    .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+  println!("cargo:rustc-env=TARGET_ARCH={}", arch);
+
+  Ok(())
+}
+
+/// Generate nanocld man page
 pub fn generate_man_command(
   name: &str,
   app: clap::Command,
@@ -23,6 +49,8 @@ pub fn generate_man_command(
 }
 
 fn main() -> std::io::Result<()> {
+  set_env_target_arch()?;
+  set_env_git_commit_hash()?;
   fs::create_dir_all(MAN_PATH)?;
   generate_man_command("nanocld", Cli::command())?;
   Ok(())
