c11d9c3fd7bf1a0f367ee1b69a68bef438c477ca
leon3s <leo.vernisse@gmail.com>
2023-04-15 22:51:47 +0200

    bugfix: install dev image

--- scripts/install_dev_image.sh
+++ scripts/install_dev_image.sh
@@ -2,7 +2,7 @@
 ## name: build_dev_image.sh
 
 docker pull cockroachdb/cockroach:v22.2.6
-docker pull nexthat/metrsd:v0.1.0
+docker pull ghcr.io/nxthat/metrsd:0.2.0
 docker pull nexthat/nanocl-get-started:latest
 docker pull ghcr.io/nxthat/nanocl-dev:dev
 docker build -t ndns:dev -f ./bin/ncddns/dnsmasq/Dockerfile ./bin/ncddns/dnsmasq
