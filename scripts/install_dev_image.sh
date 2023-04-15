c69f53770ddb0c9cf7f422e0957512a3ae177736
leon3s <leo.vernisse@gmail.com>
2023-04-15 21:40:02 +0200

    refactor: install script

--- scripts/install_dev_image.sh
+++ scripts/install_dev_image.sh
@@ -5,5 +5,5 @@ docker pull cockroachdb/cockroach:v22.2.6
 docker pull nexthat/metrsd:v0.1.0
 docker pull nexthat/nanocl-get-started:latest
 docker pull ghcr.io/nxthat/nanocl-dev:dev
-docker build -t nanocl-dns:dev -f ./bin/ncddns/dnsmasq/Dockerfile ./bin/ncddns/dnsmasq
-docker build -t nanocl-proxy:dev -f ./bin/ncdproxy/nginx/Dockerfile ./bin/ncdproxy/nginx
+docker build -t ndns:dev -f ./bin/ncddns/dnsmasq/Dockerfile ./bin/ncddns/dnsmasq
+docker build -t nproxy:dev -f ./bin/nproxy/Dockerfile .
