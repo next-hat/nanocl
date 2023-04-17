c1db8254725294a46e8bffa17cf5eaace7bb5696
leon3s <leo.vernisse@gmail.com>
2023-04-17 07:15:53 +0200

    refactor(nanocld): Better timeout when connecting to store

--- bin/nanocld/src/subsystem/store.rs
+++ bin/nanocld/src/subsystem/store.rs
@@ -1,11 +1,29 @@
-use std::time;
-use std::thread;
+use std::net::SocketAddr;
+use std::time::Duration;
+
+use ntex::rt;
+use ntex::time;
 
 use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 
 use crate::utils;
 use crate::models::Pool;
 use crate::error::CliError;
+
+async fn wait_store(addr: &str) -> Result<(), CliError> {
+  // open tcp connection
+
+  let addr: SocketAddr = addr.parse().map_err(|err| {
+    CliError::new(1, format!("Failed to parse store address: {}", err))
+  })?;
+
+  while let Err(_err) = rt::tcp_connect(addr).await {
+    log::warn!("Waiting for store");
+    time::sleep(Duration::from_millis(1000)).await;
+  }
+
+  Ok(())
+}
 
 /// Ensure existance of a container for our store
 /// we use cockroachdb with a postgresql connector.
@@ -15,13 +33,11 @@ pub(crate) async fn init(
   docker_api: &bollard_next::Docker,
 ) -> Result<Pool, CliError> {
   const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
-  let postgresql_id = utils::store::get_store_ip_addr(docker_api).await?;
-  // We wait 1000ms to ensure store is booted
-  // It's a tricky hack to avoid some error printed by postgresql connector for now.
-  log::info!("Connecting to store");
-  thread::sleep(time::Duration::from_millis(1000));
+  let store_addr = utils::store::get_store_addr(docker_api).await?;
+  log::info!("Connecting to store at: {store_addr}:26257");
+  wait_store(&format!("{store_addr}:26257")).await?;
   // Connect to postgresql
-  let pool = utils::store::create_pool(postgresql_id).await;
+  let pool = utils::store::create_pool(store_addr).await?;
   let mut conn = utils::store::get_pool_conn(&pool)?;
   log::info!("Store connected");
   // This will run the necessary migrations.
