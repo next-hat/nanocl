7bb96b20cb838b8d4b25a72109ba5332c08903a4
leon3s <leo.vernisse@gmail.com>
2023-04-25 20:53:50 +0200

    feature: middleware versioning

--- crates/nanocl_utils/src/ntex/middlewares/mod.rs
+++ crates/nanocl_utils/src/ntex/middlewares/mod.rs
@@ -1,3 +1,7 @@
-mod serialize_error;
+/// This module contains all the middlewares used by the server.
 
+mod serialize_error;
 pub use serialize_error::SerializeError;
+
+mod versioning;
+pub use versioning::Versioning;
