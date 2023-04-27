0214ade509c75bd6601b3a2eb39fa0b1671ea9c1
leon3s <leo.vernisse@gmail.com>
2023-04-27 19:46:19 +0200

    chore: upgrade tabled to 0.12.0

--- bin/nanocl/src/utils/print.rs
+++ bin/nanocl/src/utils/print.rs
@@ -1,7 +1,6 @@
-use tabled::{
-  object::{Segment, Rows},
-  Padding, Alignment, Table, Style, Modify,
-};
+use tabled::Table;
+use tabled::settings::object::{Rows, Segment};
+use tabled::settings::{Style, Modify, Padding, Alignment};
 
 use nanocl_utils::io_error::{IoResult, FromIo};
 
@@ -16,7 +15,6 @@ where
         .with(Padding::new(0, 4, 0, 0))
         .with(Alignment::left()),
     )
-    .with(Modify::new(Rows::first()).with(str::to_uppercase))
     .to_string();
   println!("{table}");
 }
