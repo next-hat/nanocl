4b1fd3b3343a5eaad96505d4121e8658a7a69cb5
leon3s <leo.vernisse@gmail.com>
2023-05-01 19:05:24 +0200

    refactor(nanocl): intaller get template by channel

--- bin/nanocl/src/utils/installer.rs
+++ bin/nanocl/src/utils/installer.rs
@@ -4,15 +4,21 @@ use nanocl_utils::io_error::{FromIo, IoResult};
 use nanocl_utils::http_error::HttpError;
 use nanocl_utils::http_client_error::HttpClientError;
 
+use crate::version::CHANNEL;
+
 /// Get template from our GitHub repo
 async fn get() -> Result<String, HttpClientError> {
   let client = http::client::Client::new();
 
-  let mut res = client
-    .get("https://raw.githubusercontent.com/nxthat/nanocl/0450e4f4066ef16b172d065e09412fba9ee49376/installer.nightly.yml")
-    .send().await
-    .map_err(|err| err
-      .map_err_context(|| "Unable to fetch installer template"))?;
+  let url = if CHANNEL == "nightly" {
+    "https://raw.githubusercontent.com/nxthat/nanocl/nightly/installer.nightly.yml"
+  } else {
+    "https://raw.githubusercontent.com/nxthat/nanocl/nightly/installer.yml"
+  };
+
+  let mut res = client.get(url).send().await.map_err(|err| {
+    err.map_err_context(|| "Unable to fetch installer template")
+  })?;
 
   let status = res.status();
   if status.is_server_error() || status.is_client_error() {
