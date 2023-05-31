8cc580fc0ba405c23581638cd42ffa9120b01d92
leon3s <leo.vernisse@gmail.com>
2023-05-31 14:37:56 +0200

    change(nanocl): cargo image ls options

--- bin/nanocld/src/models/cargo.rs
+++ bin/nanocld/src/models/cargo.rs
@@ -8,10 +8,15 @@ use super::namespace::NamespaceDbModel;
 #[diesel(table_name = cargoes)]
 #[diesel(belongs_to(NamespaceDbModel, foreign_key = namespace_name))]
 pub struct CargoDbModel {
+  /// The key of the cargo generated with `namespace_name` and `name`
   pub(crate) key: String,
+  /// The created at date
   pub(crate) created_at: chrono::NaiveDateTime,
+  /// The name of the cargo
   pub(crate) name: String,
+  /// The config key reference
   pub(crate) config_key: uuid::Uuid,
+  /// The namespace name
   pub(crate) namespace_name: String,
 }
 
@@ -19,16 +24,23 @@ pub struct CargoDbModel {
 #[derive(Debug, Default, AsChangeset)]
 #[diesel(table_name = cargoes)]
 pub struct CargoUpdateDbModel {
+  /// The key of the cargo generated with `namespace_name` and `name`
   pub(crate) key: Option<String>,
-  pub(crate) namespace_name: Option<String>,
+  /// The name of the cargo
   pub(crate) name: Option<String>,
+  /// The namespace name
+  pub(crate) namespace_name: Option<String>,
+  /// The config key reference
   pub(crate) config_key: Option<uuid::Uuid>,
 }
 
 /// Structure used to serialize cargo revert path
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct CargoRevertPath {
+  /// The version
   pub version: String,
+  /// The name
   pub name: String,
+  /// The history id
   pub id: String,
 }
