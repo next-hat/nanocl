541b4477f7b1a064cdc80edbee1d36acccfa3273
leon3s <leo.vernisse@gmail.com>
2023-09-06 15:33:45 +0200

    feature(nanocld): Add Statefile information inside openapi schemas

--- crates/nanocl_stubs/src/state.rs
+++ crates/nanocl_stubs/src/state.rs
@@ -6,74 +6,135 @@ use crate::vm_config::VmConfigPartial;
 
 use super::resource::ResourcePartial;
 
+/// ## StateMeta
+///
+/// Statefile metadata information that are always present
+///
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateMeta {
+  /// Api version to use or remote url
   pub api_version: String,
+  /// Kind of Statefile (Deployment, Cargo, VirtualMachine, Resource)
   pub kind: String,
 }
 
+/// ## StateResource
+///
+/// Statefile that represent the `Resource` kind
+///
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateResource {
+  /// List of resources to create
   pub resources: Vec<ResourcePartial>,
 }
 
+/// ## StateCargo
+///
+/// Statefile that represent the `Cargo` kind
+///
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateCargo {
+  /// Namespace where the cargoes are deployed
   pub namespace: Option<String>,
+  /// List of cargoes to create and run
   pub cargoes: Vec<CargoConfigPartial>,
 }
 
+/// ## StateVirtualMachine
+///
+/// Statefile that represent the `VirtualMachine` kind
+///
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateVirtualMachine {
+  /// Namespace where the virtual machines are deployed
   pub namespace: Option<String>,
+  /// List of virtual machines to create and run
   pub virtual_machines: Vec<VmConfigPartial>,
 }
 
+/// ## StateDeployment
+///
+/// Statefile that represent the `Deployment` kind
+///
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateDeployment {
+  /// Namespace where the cargoes and virtual machines are deployed
   pub namespace: Option<String>,
+  /// List of resources to create
   pub resources: Option<Vec<ResourcePartial>>,
+  /// List of cargoes to create and run
   pub cargoes: Option<Vec<CargoConfigPartial>>,
+  /// List of virtual machines to create and run
   pub virtual_machines: Option<Vec<VmConfigPartial>>,
 }
 
+/// ## StateStreamStatus
+///
+/// Status of a apply status for a cargo or a virtual machine
+///
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub enum StateStreamStatus {
+  /// The cargo or virtual machine is pending to be applied or removed
   Pending,
+  /// The cargo or virtual machine is failed to be applied or removed
   Failed,
+  /// The cargo or virtual machine is successfull applied or removed
   Success,
+  /// The cargo or virtual machine is not found remove is skipped
   NotFound,
+  /// The cargo or virtual machine is not changed apply is skipped
   UnChanged,
 }
 
+/// ## StateStreamKind
+///
+/// Kind of stream that is used to apply a cargo or a virtual machine
+/// This is used to know if the status of an applied or removed cargo or virtual machine
+///
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub enum StateStreamKind {
+  /// The stream is used to apply or remove a cargo
   Cargo,
+  /// The stream is used to apply or remove a virtual machine
   VirtualMachine,
+  /// The stream is used to apply or remove a resource
   Resource,
 }
 
+/// ## StateStream
+///
+/// Stream that represent the status of the apply or remove of a cargo or a virtual machine
+///
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct StateStream {
+  /// The key of the element (Cargo, VirtualMachine, Resource)
   pub key: String,
+  /// The kind of the element (Cargo, VirtualMachine, Resource)
   pub kind: String,
+  /// Some context information about the status
   pub context: Option<String>,
+  /// The status of the element (Pending, Failed, Success, NotFound, UnChanged)
   pub status: StateStreamStatus,
 }
 
