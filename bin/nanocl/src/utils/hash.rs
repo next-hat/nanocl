f2c82caf27a87f894b5758db0356d2490d0d281c
leone <leone@next-hat.com>
2023-10-23 09:10:20 +0200

    chore: upgrade to rust 1.73 (#581)

--- bin/nanocl/src/utils/hash.rs
+++ bin/nanocl/src/utils/hash.rs
@@ -20,6 +20,17 @@ pub fn calculate_SHA256(name: &str) -> String {
   hash_value
     .as_ref()
     .iter()
-    .map(|byte| format!("{:02x}", byte))
-    .collect::<String>()
+    .fold(String::new(), |acc, byte| format!("{acc}{:02x}", byte))
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_sha256() {
+    let hash = super::calculate_SHA256("test");
+    assert_eq!(
+      hash,
+      "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+    );
+  }
 }
