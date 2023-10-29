67c44c304e397cb6628c85566d9a82f3303e6e83
leon3s <leone@next-hat.com>
2023-10-29 09:30:08 +0100

    tests/nanocld: node list

--- bin/nanocld/src/services/node.rs
+++ bin/nanocld/src/services/node.rs
@@ -2,17 +2,16 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::time::Instant;
 
-use ntex::ws;
-use ntex::rt;
-use ntex::web;
+use ntex::{rt, ws, web};
 use ntex::channel::oneshot;
 use ntex::util::ByteString;
 use ntex::{Service, fn_service, chain};
 use ntex::service::{map_config, fn_shutdown, fn_factory_with_config};
 use futures::future::ready;
 
-use crate::{utils, repositories};
 use nanocl_utils::http_error::HttpError;
+
+use crate::{utils, repositories};
 use crate::models::{DaemonState, WsConState};
 
 /// List nodes
@@ -29,7 +28,6 @@ pub(crate) async fn list_node(
   state: web::types::State<DaemonState>,
 ) -> Result<web::HttpResponse, HttpError> {
   let items = repositories::node::list(&state.pool).await?;
-
   Ok(web::HttpResponse::Ok().json(&items))
 }
 
@@ -43,12 +41,10 @@ async fn node_ws_service(
   let (tx, rx) = oneshot::channel();
   let con_state = Rc::new(RefCell::new(WsConState::new()));
   rt::spawn(utils::ws::heartbeat(con_state.clone(), sink.clone(), rx));
-
   let message = format!("[SERVER] hello i'm {}", state.config.hostname);
   let _ = sink
     .send(ws::Message::Text(ByteString::from(message)))
     .await;
-
   // handler service for incoming websockets frames
   let service = fn_service(move |frame| {
     let item = match frame {
@@ -56,27 +52,20 @@ async fn node_ws_service(
         con_state.borrow_mut().hb = Instant::now();
         Some(ws::Message::Pong(msg))
       }
-      // update heartbeat
       ws::Frame::Pong(_) => {
+        // update heartbeat time
         con_state.borrow_mut().hb = Instant::now();
         None
       }
-      ws::Frame::Text(_text) => None,
-      ws::Frame::Binary(_bytes) => {
-        // println!("[SERVER] received bytes: {:#?}", bytes);
-        None
-      }
       ws::Frame::Close(reason) => Some(ws::Message::Close(reason)),
-      _ => None, // _ => Some(ws::Message::Close(None)),
+      _ => None,
     };
     ready(Ok(item))
   });
-
   // handler service for shutdown notification that stop heartbeat task
   let on_shutdown = fn_shutdown(move || {
     let _ = tx.send(());
   });
-
   // pipe our service with on_shutdown callback
   Ok(chain(service).and_then(on_shutdown))
 }
@@ -96,7 +85,7 @@ async fn node_ws(
 ) -> Result<web::HttpResponse, web::Error> {
   web::ws::start(
     req,
-    // inject chat server send to a ws_service factory
+    // inject state to the node_ws_service
     map_config(fn_factory_with_config(node_ws_service), move |cfg| {
       (cfg, state.clone())
     }),
@@ -107,4 +96,27 @@ async fn node_ws(
 pub fn ntex_config(config: &mut web::ServiceConfig) {
   config.service(list_node);
   config.service(web::resource("/nodes/ws").route(web::get().to(node_ws)));
+}
+
+#[cfg(test)]
+mod tests {
+
+  use ntex::http;
+
+  use nanocl_stubs::node::Node;
+  use nanocl_utils::ntex::test_client::test_status_code;
+
+  use crate::services::ntex_config;
+  use crate::version::VERSION;
+  use crate::utils::tests::*;
+
+  const ENDPOINT: &str = "/nodes";
+
+  #[ntex::test]
+  async fn basic() {
+    let client = generate_test_client(ntex_config, VERSION).await;
+    let mut res = client.send_get(ENDPOINT, None::<String>).await;
+    test_status_code!(res.status(), http::StatusCode::OK, "list nodes");
+    let _ = res.json::<Vec<Node>>().await.unwrap();
+  }
 }
