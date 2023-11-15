61564e91080a5cb1ca5c952a61bc487a39445488
leon3s <leone@next-hat.com>
2023-11-15 22:40:31 +0100

    add more nanocld_client tests

--- crates/nanocld_client/src/job.rs
+++ crates/nanocld_client/src/job.rs
@@ -1,6 +1,8 @@
 use ntex::channel::mpsc::Receiver;
 
-use nanocl_stubs::job::{Job, JobLogOutput, JobWaitQuery, JobWaitResponse};
+use nanocl_stubs::job::{
+  Job, JobLogOutput, JobWaitQuery, JobWaitResponse, JobPartial,
+};
 use nanocl_error::http::HttpError;
 use nanocl_error::http_client::HttpClientError;
 
@@ -50,6 +52,15 @@ impl NanocldClient {
   ///   * [Ok](Ok) - [Receiver](Receiver) of [Result](Result) of [JobLogOutput](JobLogOutput) if operation was successful
   ///   * [Err](Err) - [Http client error](HttpClientError) if operation failed
   ///
+  /// ## Example
+  ///
+  /// ```no_run,ignore
+  /// use nanocld_client::NanocldClient;
+  ///
+  /// let client = NanocldClient::connect_to("http://localhost:8585", None);
+  /// let stream = client.logs_job("my_job").await.unwrap();
+  /// ```
+  ///
   pub async fn logs_job(
     &self,
     name: &str,
@@ -58,6 +69,82 @@ impl NanocldClient {
       .send_get(&format!("{}/{name}/logs", Self::JOB_PATH), None::<String>)
       .await?;
     Ok(Self::res_stream(res).await)
+  }
+
+  /// ## Create a job
+  ///
+  /// Create a job from a [JobPartial](JobPartial)
+  ///
+  /// ## Arguments
+  ///
+  /// * [job](JobPartial) - The job to create
+  ///
+  /// ## Returns
+  ///
+  /// * [Result](Result) - The result of the operation
+  ///   * [Ok](Ok) - [Job](Job) if operation was successful
+  ///   * [Err](Err) - [Http client error](HttpClientError) if operation failed
+  ///
+  /// ## Example
+  ///
+  /// ```no_run,ignore
+  /// use nanocld_client::NanocldClient;
+  /// use nanocld_client::bollard_next::container::Config;
+  ///
+  /// let client = NanocldClient::connect_to("http://localhost:8585", None);
+  /// let res = client.create_job(&JobPartial {
+  ///  name: "my_job".to_string(),
+  ///  containers: vec![
+  ///   Config {
+  ///     image: Some("alpine:latest".to_string()),
+  ///     cmd: Some(vec!["echo".to_string(), "Hello world".to_string()]),
+  ///   }
+  ///  ],
+  /// }).await;
+  /// ```
+  ///
+  pub async fn create_job(
+    &self,
+    job: &JobPartial,
+  ) -> Result<Job, HttpClientError> {
+    let res = self
+      .send_post(Self::JOB_PATH, Some(job.clone()), None::<String>)
+      .await?;
+    Self::res_json(res).await
+  }
+
+  /// ## Start a job
+  ///
+  /// Start a job by it's name
+  ///
+  /// ## Arguments
+  ///
+  /// * [name](str) - The name of the job to start
+  ///
+  /// ## Returns
+  ///
+  /// * [Result](Result) - The result of the operation
+  ///   * [Ok](Ok) - [()] if operation was successful
+  ///   * [Err](Err) - [Http client error](HttpClientError) if operation failed
+  ///
+  /// ## Example
+  ///
+  /// ```no_run,ignore
+  /// use nanocld_client::NanocldClient;
+  ///
+  /// let client = NanocldClient::connect_to("http://localhost:8585", None);
+  /// let res = client.start_job("my_job").await;
+  /// ```
+  ///
+  pub async fn start_job(&self, name: &str) -> Result<(), HttpClientError> {
+    self
+      .send_post(
+        &format!("{}/{name}/start", Self::JOB_PATH),
+        None::<String>,
+        None::<String>,
+      )
+      .await?;
+    Ok(())
   }
 
   /// ## Wait a job
@@ -70,6 +157,21 @@ impl NanocldClient {
   /// * [name](str) - The name of the job to get the logs
   /// * [query](Option) - Optional [query](JobWaitQuery)
   ///
+  /// ## Returns
+  ///
+  /// * [Result](Result) - The result of the operation
+  ///   * [Ok](Ok) - [Receiver](Receiver) of [Result](Result) of [JobWaitResponse](JobWaitResponse) if operation was successful
+  ///   * [Err](Err) - [Http client error](HttpClientError) if operation failed
+  ///
+  /// ## Example
+  ///
+  /// ```no_run,ignore
+  /// use nanocld_client::NanocldClient;
+  ///
+  /// let client = NanocldClient::connect_to("http://localhost:8585", None);
+  /// let stream = client.wait_job("my_job", None).await.unwrap();
+  /// ```
+  ///
   pub async fn wait_job(
     &self,
     name: &str,
@@ -80,19 +182,76 @@ impl NanocldClient {
       .await?;
     Ok(Self::res_stream(res).await)
   }
+
+  /// ## Delete a job
+  ///
+  /// Delete a job by it's name
+  ///
+  /// ## Arguments
+  ///
+  /// * [name](str) - The name of the job to delete
+  ///
+  /// ## Returns
+  ///
+  /// * [Result](Result) - The result of the operation
+  ///   * [Ok](Ok) - If operation was successful
+  ///   * [Err](Err) - [Http client error](HttpClientError) if operation failed
+  ///
+  /// ## Example
+  ///
+  /// ```no_run,ignore
+  /// use nanocld_client::NanocldClient;
+  ///
+  /// let client = NanocldClient::connect_to("http://localhost:8585", None);
+  /// let res = client.delete_job("my_job").await;
+  /// ```
+  ///
+  pub async fn delete_job(&self, name: &str) -> Result<(), HttpClientError> {
+    self
+      .send_delete(&format!("{}/{name}", Self::JOB_PATH), None::<String>)
+      .await?;
+    Ok(())
+  }
 }
 
 #[cfg(test)]
 mod tests {
+  use bollard_next::container::Config;
+  use futures::StreamExt;
+
   use super::*;
-  use nanocl_error::http_client::HttpClientError;
 
   #[ntex::test]
-  async fn list_job() -> Result<(), HttpClientError> {
+  async fn list_job() {
     let client =
       NanocldClient::connect_to("http://ndaemon.nanocl.internal:8585", None);
     let res = client.list_job().await;
     assert!(res.is_ok());
-    Ok(())
+  }
+
+  #[ntex::test]
+  async fn basic() {
+    let client =
+      NanocldClient::connect_to("http://ndaemon.nanocl.internal:8585", None);
+    let job = client
+      .create_job(&JobPartial {
+        name: "my_test_job".to_owned(),
+        containers: vec![Config {
+          image: Some("alpine:latest".to_owned()),
+          cmd: Some(vec!["echo".to_owned(), "Hello world".to_owned()]),
+          ..Default::default()
+        }],
+        secrets: None,
+        metadata: None,
+      })
+      .await
+      .unwrap();
+    assert_eq!(job.name, "my_test_job");
+    let mut stream = client.wait_job(&job.name, None).await.unwrap();
+    client.start_job(&job.name).await.unwrap();
+    while let Some(Ok(_)) = stream.next().await {}
+    let mut stream = client.logs_job(&job.name).await.unwrap();
+    while let Some(Ok(_)) = stream.next().await {}
+    client.delete_job(&job.name).await.unwrap();
   }
 }
