5a68dc57cd404a1781ecb4298be556977758c7ba
leone <leone@next-hat.com>
2023-12-09 21:53:43 +0100

    feature: job logs options (#708)

--- bin/nanocl/src/models/job.rs
+++ bin/nanocl/src/models/job.rs
@@ -44,10 +44,26 @@ pub struct JobInspectOpts {
   pub name: String,
 }
 
+/// `nanocl job logs` available options
 #[derive(Parser)]
 pub struct JobLogsOpts {
-  /// Name of job to inspect
+  /// Name of job to show logs
   pub name: String,
+  /// Only include logs since unix timestamp
+  #[clap(short = 's')]
+  pub since: Option<i64>,
+  /// Only include logs until unix timestamp
+  #[clap(short = 'u')]
+  pub until: Option<i64>,
+  /// If integer only return last n logs, if "all" returns all logs
+  #[clap(short = 't')]
+  pub tail: Option<String>,
+  /// Bool, if set include timestamp to ever log line
+  #[clap(long = "timestamps")]
+  pub timestamps: bool,
+  /// Bool, if set open the log as stream
+  #[clap(short = 'f')]
+  pub follow: bool,
 }
 
 /// `nanocl job` available commands
