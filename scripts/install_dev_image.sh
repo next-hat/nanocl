fee93be622582a89a9cebf878cd32a24acea4d66
leon3s <leo.vernisse@gmail.com>
2023-12-17 14:04:14 +0100

    refactor vm image

--- scripts/install_dev_image.sh
+++ scripts/install_dev_image.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 ## name: build_dev_image.sh
 
-docker pull cockroachdb/cockroach:v23.1.12
-docker pull ghcr.io/nxthat/metrsd:0.3.2
-docker pull ghcr.io/nxthat/nanocl-get-started:latest
-docker pull ghcr.io/nxthat/nanocl-dev:dev
-docker build --network host -t ndns:dev -f ./bin/ndns/Dockerfile .
-docker build --network host -t nproxy:dev -f ./bin/nproxy/Dockerfile .
+wget https://cloud-images.ubuntu.com/minimal/releases/jammy/release/ubuntu-22.04-minimal-cloudimg-amd64.img
+mv ubuntu-22.04-minimal-cloudimg-amd64.img tests/ubuntu-22.04-minimal-cloudimg-amd64.img
+
+# docker pull cockroachdb/cockroach:v23.1.12
+# docker pull ghcr.io/nxthat/metrsd:0.3.2
+# docker pull ghcr.io/nxthat/nanocl-get-started:latest
+# docker pull ghcr.io/nxthat/nanocl-dev:dev
+# docker build --network host -t ndns:dev -f ./bin/ndns/Dockerfile .
+# docker build --network host -t nproxy:dev -f ./bin/nproxy/Dockerfile .
