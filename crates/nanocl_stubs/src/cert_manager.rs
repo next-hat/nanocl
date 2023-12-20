d10133a74c551cf0d1613231f5d3061e9fe40778
anonkey <6380129+anonkey@users.noreply.github.com>
2023-12-20 13:43:01 +0100

    p

--- crates/nanocl_stubs/src/cert_manager.rs
+++ crates/nanocl_stubs/src/cert_manager.rs
@@ -1,12 +1,12 @@
 use serde::{Serialize, Deserialize};
 
-use crate::cargo_config::CargoConfigPartial;
+use crate::cargo_spec::CargoSpecPartial;
 
-/// Defines a CertManagerIssuer config
+/// Defines a CertManagerIssuer spec
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "PascalCase"))]
 pub struct CertManagerIssuer {
-  /// Certificate generation CargoConfig
-  pub generate: CargoConfigPartial,
+  /// Certificate generation CargoSpec
+  pub generate: CargoSpecPartial,
 }
