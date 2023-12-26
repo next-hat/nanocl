bcf1c4b720442b8cb45a09904fd9c3d3559ae083
leon3s <leo.vernisse@gmail.com>
2023-12-26 17:35:55 +0100

    fix tests

--- scripts/prepare_test_ci.sh
+++ scripts/prepare_test_ci.sh
@@ -2,13 +2,13 @@ docker run -i --rm \
   -v $(pwd):/project \
   -v $HOME/.cargo/registry:/usr/local/cargo/registry \
   ghcr.io/next-hat/nanocl-dev:dev \
-  build --no-default-features --features "dev" --bin nanocld
+  build --bin nanocld --no-default-features --features "dev"
 
 docker run -i --rm \
   -v $(pwd):/project \
   -v $HOME/.cargo/registry:/usr/local/cargo/registry \
   ghcr.io/next-hat/nanocl-dev:dev \
-  build --no-default-features --features "dev" --bin ncproxy
+  build --bin ncproxy --no-default-features --features "dev"
 
 docker run -d --rm \
   -v $(pwd):/project \
@@ -21,7 +21,7 @@ docker run -d --rm \
   -w /project \
   --network host \
   ghcr.io/next-hat/nanocl-dev:dev \
-  run --no-default-features --features "dev" --bin nanocld -- --hosts tcp://0.0.0.0:8585 --state-dir $HOME/.nanocl_dev/state
+  run --bin nanocld --no-default-features --features "dev" -- --hosts tcp://0.0.0.0:8585 --state-dir $HOME/.nanocl_dev/state
 
 docker run -d --rm \
   -v $(pwd):/project \
@@ -34,4 +34,4 @@ docker run -d --rm \
   -w /project \
   --network host \
   ghcr.io/next-hat/nanocl-dev:dev \
-  run --no-default-features --features "dev" --bin ncproxy -- --state-dir $HOME/.nanocl_dev/state
+  run --bin ncproxy --no-default-features --features "dev" -- --state-dir $HOME/.nanocl_dev/state/proxy
