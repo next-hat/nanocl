9d403526d015b56b012725f20ce681edb3fb3f01
leon3s <leo.vernisse@gmail.com>
2023-12-27 13:52:13 +0100

    refactor/ncdns: with state dir

--- bin/ndns/entrypoint.sh
+++ bin/ndns/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# Test if a config file exists at /opt/dns/dnsmasq.conf
-if [ ! -f /opt/dns/dnsmasq.conf ]; then
-  echo "No config file found at /opt/dns/dnsmasq.conf loading default config"
-  mv /dnsmasq.conf /opt/dns/dnsmasq.conf
+if [ -z "$STATE_DIR" ]; then
+  echo "STATE_DIR env is not set"
+  exit 1
 fi
 
-# Test if directory /opt/dns/dnsmasq.d exists
-if [ ! -d /opt/dns/dnsmasq.d ]; then
-  echo "No directory found at /opt/dns/dnsmasq.d creating directory"
-  mkdir /opt/dns/dnsmasq.d
+# Test if a config file exists at $STATE_DIR/dnsmasq.conf
+if [ ! -f $STATE_DIR/dnsmasq.conf ]; then
+  echo "No config file found at $STATE_DIR/dnsmasq.conf loading default config"
+  cp /dnsmasq.conf $STATE_DIR/dnsmasq.conf
+fi
+
+# Test if directory $STATE_DIR/dnsmasq.d exists
+if [ ! -d $STATE_DIR/dnsmasq.d ]; then
+  echo "No directory found at $STATE_DIR/dnsmasq.d creating directory"
+  mkdir $STATE_DIR/dnsmasq.d
 fi
 
 # Start dnsmasq
-dnsmasq -k -C /opt/dns/dnsmasq.conf --log-facility=-
+dnsmasq -k -C $STATE_DIR/dnsmasq.conf --log-facility=-
