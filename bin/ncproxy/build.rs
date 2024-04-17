1d264d1da6472b32dfa35902d2fd10932c9fa969
leone <leone@next-hat.com>
2024-04-17 14:40:06 +0200

    refactor: remove useless man page for ncproxy, ncdns and nanocld (#889)
    * refactor: remove useless man page for ncproxy, ncdns and nanocld
    
    * remove markdown files

--- bin/ncproxy/build.rs
+++ bin/ncproxy/build.rs
@@ -1,16 +1,8 @@
-use clap::*;
-
 use nanocl_utils::build_tools::*;
-
-include!("./src/cli.rs");
-
-const MAN_PATH: &str = "./target/man";
 
 fn main() -> std::io::Result<()> {
   set_channel()?;
   set_env_target_arch()?;
   set_env_git_commit_hash()?;
-  std::fs::create_dir_all(MAN_PATH)?;
-  generate_man_page("ncproxy", &Cli::command(), MAN_PATH)?;
   Ok(())
 }
