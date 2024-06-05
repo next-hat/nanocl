ebf28e01435535e553797ff7d9bfc3610b2d7433
leon3s <leo.vernisse@gmail.com>
2024-06-05 22:07:41 +0200

    fix/nanocl: cargo revert wait for the update

--- bin/nanocl/src/main.rs
+++ bin/nanocl/src/main.rs
@@ -182,7 +182,7 @@ mod tests {
       .clone();
     assert_cli_ok!("cargo", "revert", CARGO_NAME, &history.key.to_string());
     // Try to stop a cargo
-    // assert_cli_ok!("cargo", "stop", CARGO_NAME);
+    assert_cli_ok!("cargo", "stop", CARGO_NAME);
     // Try to remove cargo
     assert_cli_ok!("cargo", "rm", "-fy", CARGO_NAME);
   }
