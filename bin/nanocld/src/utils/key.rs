6caee75303fbe41fed7120da571d49f12d74b882
leon3s <leo.vernisse@gmail.com>
2026-02-17 10:46:26 +0100

    chore: update rand to 0.10

--- bin/nanocld/src/utils/key.rs
+++ bin/nanocld/src/utils/key.rs
@@ -3,7 +3,7 @@
 /// or on the key of the parent for relational purpose.
 /// For example if we create a cargo `get-started` in the default namespace `global`
 /// The cargo key will be `get-started.global`
-use rand::{Rng, distr::Alphanumeric, rng};
+use rand::{RngExt, distr::Alphanumeric, rng};
 
 use nanocl_error::{
   http::{HttpError, HttpResult},
